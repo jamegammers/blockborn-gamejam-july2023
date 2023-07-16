9ced73b26dab733197b7e99349d30c07e2d99a11
mertyn <mweber28@hs-mittweida.de>
2023-07-16 11:31:09 +0200

    Added button behaviour

--- blockborn-gamejam-july2023/Assets/Scripts/ArcadeMachine/ArcadeButtons.cs
+++ blockborn-gamejam-july2023/Assets/Scripts/ArcadeMachine/ArcadeButtons.cs
@@ -1,20 +1,32 @@
 using System;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Vector2 = System.Numerics.Vector2;
 
 namespace ArcadeMachine {
 
-    public enum ArcadeButtonType { Joystick, Button }
+    public enum ArcadeButtonType { Button, Joystick }
 
     [Serializable]
     public struct ArcadeButton {
         
+        // references
+        [SerializeField] private string _name;
         [SerializeField] private Transform _object;
-        [SerializeField] private ArcadeButtonType _type;
+        [SerializeField, EnumToggleButtons] private ArcadeButtonType _type;
         [SerializeField] private InputActionReference _inputAction;
         
+        // settings
+        [SerializeField, ShowIf("_type", ArcadeButtonType.Button)] private float _offsetPressed;
+        [SerializeField, ShowIf("_type", ArcadeButtonType.Joystick)] private float _rotationOffsetPressed;
+        
+        // property getters
         public Transform Object => _object;
         public ArcadeButtonType Type => _type;
+        public InputActionReference InputAction => _inputAction;
+        public float OffsetPressed => _offsetPressed;
+        public float RotationOffsetPressed => _rotationOffsetPressed;
         
     }
 
@@ -27,8 +39,32 @@ namespace ArcadeMachine {
                 RegisterEvents(button);
         }
 
-        private void RegisterEvents(ArcadeButton button) {
-            
+        private static void RegisterEvents(ArcadeButton button) {
+            switch (button.Type) {
+                case ArcadeButtonType.Button:
+                    button.InputAction.action.started += _ =>
+                        button.Object.localPosition = new Vector3(0, -button.OffsetPressed, 0);
+                
+                    button.InputAction.action.canceled += _ =>
+                        button.Object.localPosition = Vector3.zero;
+                    
+                    button.InputAction.action.Enable();
+                    break;
+
+                case ArcadeButtonType.Joystick:
+                    button.InputAction.action.started += ctx => {
+                        Vector2 dir = ctx.ReadValue<Vector2>();
+                    
+                        // z = x
+                        // x = y
+                    
+                        button.Object.localRotation = Quaternion.Euler(0, 0, 0);
+                    };
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
         
     }
