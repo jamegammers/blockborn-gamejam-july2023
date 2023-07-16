584529c15e5c1406ccdf765aa3c2b0266ccb93af
Benny <berl@hs-mittweida.de>
2023-07-16 12:49:43 +0200

    player prefab + scene cleanup

--- blockborn-gamejam-july2023/Assets/Scripts/LevelGeneration/LevelGeneration.cs
+++ blockborn-gamejam-july2023/Assets/Scripts/LevelGeneration/LevelGeneration.cs
@@ -8,13 +8,21 @@ public class LevelGeneration : MonoBehaviour
 {
     [SerializeField] private GameObject[] _levelTiles;
     private NewInputActionMap _inputActionMap;
-    private int counter;
-
+    
+    private int counter = 1;
+    
+    private Transform _levelHolder;
+    
     private void Awake()
     {
         _inputActionMap = new NewInputActionMap();
         _inputActionMap.PlayerTesting.Enable();
         _inputActionMap.PlayerTesting.Test.performed += context =>  GenerateLevel();
+    }
+
+    private void Start()
+    {
+        _levelHolder = GameObject.Find("LevelHolder").transform;
     }
 
     private void GenerateLevel()
@@ -23,7 +31,7 @@ public class LevelGeneration : MonoBehaviour
         Vector3 position = new Vector3(counter * 25, 0, 0);
         
         // Instaniate a random tile from the array of tiles using the RandomizeNumbers method from the Randomize class
-        Instantiate(_levelTiles[GetComponent<Randomize>().RandomizeNumbers(0, _levelTiles.Length)], position, Quaternion.identity);
+        Instantiate(_levelTiles[GetComponent<Randomize>().RandomizeNumbers(0, _levelTiles.Length)], position, Quaternion.identity).transform.parent = _levelHolder;
         counter++;
     }
 
