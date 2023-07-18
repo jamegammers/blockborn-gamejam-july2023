dd2ef0b1382904a631e46fc5a86eb15e370424d9
mertyn <mweber28@hs-mittweida.de>
2023-07-18 11:36:26 +0200

    Started adding highscores list

---
+++ blockborn-gamejam-july2023/Assets/Scripts/UI/HighscoresList.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace UI {
+    public class HighscoresList : MonoBehaviour {
+
+        public void Show(int newScore) {
+            // load scores, fill list, make visible...
+            // check if new score is in list
+            // - true: name input + save
+        }
+
+    }
+}
