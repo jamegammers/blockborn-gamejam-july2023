53e2a53d9d2d204d73b9b2871179c7c53f51a937
Fennia <fweidne2@hs-mittweida.de>
2023-07-23 14:16:59 +0200

    Background Spawning

--- blockborn-gamejam-july2023/Assets/Scripts/LevelGeneration/BuildingsNum.cs
+++ blockborn-gamejam-july2023/Assets/Scripts/LevelGeneration/BuildingsNum.cs
@@ -6,4 +6,7 @@ public class BuildingsNum : MonoBehaviour
 {
     [SerializeField] private int buildHeight;
     [SerializeField] private int buildWidth;
+    
+    public int buildheight { get { return buildHeight; } }
+    public int buildwidth { get { return buildWidth; } }
 }
