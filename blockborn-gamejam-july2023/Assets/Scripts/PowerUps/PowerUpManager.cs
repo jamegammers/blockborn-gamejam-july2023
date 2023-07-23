9e4ce9945b6f122890684e9b7ef7c8eb63cbf56d
Benny <berl@hs-mittweida.de>
2023-07-23 21:32:09 +0200

    ✨TrulyRandomized PowerUps

--- blockborn-gamejam-july2023/Assets/Scripts/PowerUps/PowerUpManager.cs
+++ blockborn-gamejam-july2023/Assets/Scripts/PowerUps/PowerUpManager.cs
@@ -11,11 +11,30 @@ public class PowerUpManager : MonoBehaviour
     [Space]
     [SerializeField] private GameObject[] _powerUpList;
 
+    private int _spawnChance = 11;
+
     public void InstanciatePowerUp(Vector3 spawnPos, GameObject powerUpSpawnLocation)
     {
-        // pick a random power up from the list
-        Instantiate(_powerUpList[_randomize.RandomizeNumbers(0, _powerUpList.Length)], 
-            spawnPos, Quaternion.identity).transform.parent = powerUpSpawnLocation.transform;
+        
+        Debug.Log(_randomize.RandomizeNumbers(0, 0));
+        
+        // if randomize number is 0, spawn a power up
+        if (_randomize.RandomizeNumbers(0, _spawnChance) == 0)
+        {
+            // pick a random power up from the list
+            Instantiate(_powerUpList[_randomize.RandomizeNumbers(0, _powerUpList.Length)], 
+                spawnPos, Quaternion.identity).transform.parent = powerUpSpawnLocation.transform;
+            
+            _spawnChance = 11;
+        } 
+        
+        // else, increase spawn chance
+        else
+        {
+            if (_spawnChance !< 1)
+            {
+                _spawnChance--;
+            }
+        }
     }
-    
 }
