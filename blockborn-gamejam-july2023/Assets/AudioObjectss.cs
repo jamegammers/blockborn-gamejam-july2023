db09e801fb70e6b9ffc3507243dbc0d1dcb94263
Jannik B <bge.jannik@gmail.com>
2023-07-24 16:05:20 +0200

    saunds

---
+++ blockborn-gamejam-july2023/Assets/AudioObjectss.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Audio;
+
+public class AudioObjectss : MonoBehaviour
+{
+    [SerializeField] private Dictionary<string, AudioSample> AudioSamples;
+
+    private void Start()
+    {
+
+    }
+}
---
+++ blockborn-gamejam-july2023/Assets/Scripts/AudioObjects.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using Audio;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioObjects : MonoBehaviour
+{
+    [SerializeField] private List<AudioSample> AudioSamples;
+
+}
