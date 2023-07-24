bdc2fe153adc2b72f9371e5dcbbe8ea64a775c04
Jannik B <bge.jannik@gmail.com>
2023-07-24 16:12:01 +0200

    more sounds

--- blockborn-gamejam-july2023/Assets/Scripts/Player/AbilityPickUp.cs
+++ blockborn-gamejam-july2023/Assets/Scripts/Player/AbilityPickUp.cs
@@ -2,16 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Audio;
 
 public class AbilityPickUp : MonoBehaviour
 {
    [SerializeField] private Abilities.Ability ability;
+    [SerializeField] private AudioSample _audioPowerUp;
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == 6)
         {
-            Debug.Log("Player found pickUp");
+            ArcadeAudio.PlayAudio(_audioPowerUp);
             other.GetComponent<Abilities>().SetAbility(ability);
             Destroy(gameObject);
         }
